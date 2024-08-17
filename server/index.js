7bfd94cf7680e453f1c43b16d9302422976d3607
santoshini <santoshinipradhan01@gmail.com>
2024-08-17 09:52:26 +0530

    added signup/signin

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { mongoose } = require('mongoose');
-
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/userRoute');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT =  3000
 
+app.use(bodyParser.json());
 app.use(express.json())
 
+app.use('/user',userRouter);
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log('mongodb connected');
